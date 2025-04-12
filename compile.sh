d286b56e06ba81b2292651c01b10d147f01f1078
KachleGG <drztesiklobouky822@gmail.com>
2025-04-12 12:28:01 +0200

    1.1.0

--- compile.sh
+++ compile.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
+proj_name="IpAddr"
 version=$(cat version.txt)
 
-cd IpAddr
+cd $proj_name
 
 # Win:
-dotnet publish -c Release -r win-x64 --self-contained true /p:PublishTrimmed=true /p:TrimMode=link /p:PublishSingleFile=true /p:InvariantGlobalization=true /p:DebugType=None /p:DebugSymbols=false /p:AssemblyName="IpAddr_win-x64-$version"
+dotnet publish -c Release -r win-x64 --self-contained true /p:PublishTrimmed=true /p:TrimMode=link /p:PublishSingleFile=true /p:InvariantGlobalization=true /p:DebugType=None /p:DebugSymbols=false /p:AssemblyName="$proj_name-win-x64-$version"
 
 # Lin:
-dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishTrimmed=true /p:TrimMode=link /p:PublishSingleFile=true /p:InvariantGlobalization=true /p:DebugType=None /p:DebugSymbols=false /p:AssemblyName="IpAddr_linux-x64-$version"
+dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishTrimmed=true /p:TrimMode=link /p:PublishSingleFile=true /p:InvariantGlobalization=true /p:DebugType=None /p:DebugSymbols=false /p:AssemblyName="$proj_name-linux-x64-$version"
 
 # masOS:
-dotnet publish -c Release -r osx-x64 --self-contained true /p:PublishTrimmed=true /p:TrimMode=link /p:PublishSingleFile=true /p:InvariantGlobalization=true /p:DebugType=None /p:DebugSymbols=false /p:AssemblyName="IpAddr_osx-x64-$version"
+dotnet publish -c Release -r osx-x64 --self-contained true /p:PublishTrimmed=true /p:TrimMode=link /p:PublishSingleFile=true /p:InvariantGlobalization=true /p:DebugType=None /p:DebugSymbols=false /p:AssemblyName="$proj_name-osx-x64-$version"
 
 # Returning to the main directory
 cd ..
 
 # Copy executables to the main directory
-cp IpAddr/bin/Release/net8.0/win-x64/publish/IpAddr_win-x64-$version.exe ./
-cp IpAddr/bin/Release/net8.0/linux-x64/publish/IpAddr_linux-x64-$version ./
-cp IpAddr/bin/Release/net8.0/osx-x64/publish/IpAddr_osx-x64-$version ./
+cp $proj_name/bin/Release/net8.0/win-x64/publish/$proj_name-win-x64-$version.exe ./
+cp $proj_name/bin/Release/net8.0/linux-x64/publish/$proj_name-linux-x64-$version ./
+cp $proj_name/bin/Release/net8.0/osx-x64/publish/$proj_name-osx-x64-$version ./
 
 # Cleaning up the install
-rm -rf IpAddr/bin/*
-rm -rf IpAddr/obj/*
+rm -rf $proj_name/bin/*
+rm -rf $proj_name/obj/*
